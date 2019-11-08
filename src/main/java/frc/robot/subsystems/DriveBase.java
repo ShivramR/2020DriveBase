e79219ca98862fc8f999084b707eb27cc748ea9d
Shivram <sramkumar@gsgis.k12.va.us>
2019-11-08 18:17:46 -0500

    This one works

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -1,54 +1,58 @@
 package frc.robot.subsystems;
 
-import frc.robot.RobotMap;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.commands.other.TankDrive;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.TankDrive;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.SPI;
-import frc.robot.DifferentialDriveCorrection;
-
 
 public class DriveBase extends Subsystem {
 
-    public WPI_TalonSRX leftMiddleMaster;
-    public WPI_TalonSRX rightMiddleMaster;
+    private WPI_TalonSRX leftMasterMotor;
+    private WPI_TalonSRX rightMasterMotor;
 
-    public WPI_VictorSPX leftFrontFollower;
-    public WPI_VictorSPX leftRearFollower;
-    public WPI_VictorSPX rightFrontFollower;
-    public WPI_VictorSPX rightRearFollower;        
+    private WPI_VictorSPX leftFollower1;
+    private WPI_VictorSPX leftFollower2;
+    private WPI_VictorSPX rightFollower1;
+    private WPI_VictorSPX rightFollower2;
 
-    public ADXRS450_Gyro gyro;
-    private SpeedControllerGroup leftSide;
-    private SpeedControllerGroup rightSide;
-    public DifferentialDrive cheesyDrive; 
-    private static final SPI.Port kGyroPort = SPI.Port.kOnboardCS0;
+    private ADXRS450_Gyro gyro;
+
+    SpeedControllerGroup leftSide;
+    SpeedControllerGroup rightSide;
+
+    public DifferentialDrive splitDrive;
+    public DifferentialDrive arcadeDrive;
+    public DifferentialDrive cheesyDrive;
 
     public DriveBase() {
         super("DriveBase");
-        this.leftMiddleMaster = new WPI_TalonSRX(RobotMap.leftMiddleMaster); 
-        this.rightMiddleMaster = new WPI_TalonSRX(RobotMap.rightMiddleMaster);
-        
-        this.leftFrontFollower = new WPI_VictorSPX(RobotMap.leftFrontFollower);
-        this.leftRearFollower = new WPI_VictorSPX(RobotMap.leftRearFollower);
-        this.rightFrontFollower = new WPI_VictorSPX(RobotMap.rightFrontFollower);
-        this.rightRearFollower = new WPI_VictorSPX(RobotMap.rightRearFollower);
+        this.leftMasterMotor = new WPI_TalonSRX(RobotMap.leftMasterMotor);
+        this.rightMasterMotor = new WPI_TalonSRX(RobotMap.rightMasterMotor);
 
-        leftMiddleMaster.setInverted(true);
-        leftFrontFollower.setInverted(true);
-        leftRearFollower.setInverted(true);
+        this.leftFollower1 = new WPI_VictorSPX(RobotMap.leftFollower1);
+        this.leftFollower2 = new WPI_VictorSPX(RobotMap.leftFollower2);
+        this.rightFollower1 = new WPI_VictorSPX(RobotMap.rightFollower1);
+        this.rightFollower2 = new WPI_VictorSPX(RobotMap.rightFollower2);
 
-        this.gyro = new ADXRS450_Gyro(kGyroPort);
-        this.leftSide = new SpeedControllerGroup(leftMiddleMaster, leftFrontFollower, leftRearFollower);
-        this.rightSide = new SpeedControllerGroup(rightMiddleMaster, rightFrontFollower, rightRearFollower);        
+        this.gyro = new ADXRS450_Gyro();
+
+        this.leftSide = new SpeedControllerGroup(leftMasterMotor, leftFollower1, leftFollower2);
+        this.rightSide = new SpeedControllerGroup(rightMasterMotor, rightFollower1, rightFollower2);
+
+        this.splitDrive = new DifferentialDrive(leftSide, rightSide); 
+        this.arcadeDrive = new DifferentialDrive(leftSide, rightSide);
         this.cheesyDrive = new DifferentialDrive(leftSide, rightSide);
+        
+        leftMasterMotor.setInverted(true);
+        leftFollower1.setInverted(true);
+        leftFollower2.setInverted(true);
 
     }
 
-    public void initDefaultCommand() {this.setDefaultCommand(new TankDrive());}
-
+    @Override
+    public void initDefaultCommand() { this.setDefaultCommand(new TankDrive()); }
 }
